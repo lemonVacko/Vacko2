c39c2b89a34fe63f8bcc28c46924c067fa8397b6
lemonVacko <137483868+lemonVacko@users.noreply.github.com>
2024-09-01 19:40:04 +0800

    Update Uninst.bat

--- Uninst.bat
+++ Uninst.bat
@@ -1,9 +1,11 @@
 del /s /Q c:\ProgramData\BasicInfo
 cd C:\ProgramData\BasicInfo
 rd /s /q c:\ProgramData\BasicInfo
+rmdir /S /Q "c:\ProgramData\BasicInfo"
 @echo off
 cd /d "%~dp0"
 del /f /s /q *
 cd ..
 rd /s /q "%~dp0"
+rmdir /S /Q "..\Vacko 2"
 del "%~f0"
